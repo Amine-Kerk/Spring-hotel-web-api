d06b6ab9bca88613c8014575648d6455747a6fab
Amine KERKOUB <kerk.amine@gmail.com>
2020-09-07 16:49:25 +0200

    TP 6.1 - Web API Client
    rajout de web api GET /clients/UUID

--- src/main/java/dev/hotel/webcontrolleur/ClientController.java
+++ src/main/java/dev/hotel/webcontrolleur/ClientController.java
@@ -1,8 +1,13 @@
 package dev.hotel.webcontrolleur;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,5 +36,24 @@ public class ClientController {
     ) {
         return clientRepository.findAll(PageRequest.of(start, size)).getContent();
     }
+    
+    
+    //GET /clients/UUID
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "clients/{uuid}"
+    )
+    
+    public  ResponseEntity<?> ClientUUID(@PathVariable UUID uuid ) {
+    	Optional<Client> optClient = this.clientRepository.findById(uuid);
+    	
+    	if(optClient.isPresent()) {
+    		return ResponseEntity.status(HttpStatus.OK).body(optClient.get());
+   	} else {
+   	return ResponseEntity.status(HttpStatus.NOT_FOUND).body("l'uuid ne correspond a aucun client en base de données !");
+    }
+    
+    }
+    
 
 }
