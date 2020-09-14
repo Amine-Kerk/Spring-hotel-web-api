7766147a78aba0e4fd54e6d254c1d88c12708e01
Amine KERKOUB <kerk.amine@gmail.com>
2020-09-14 16:00:44 +0200

    TP 6.2 - Web API Reservation
    V1.4

--- src/test/java/dev/hotel/web/ClientControllerTest.java
+++ src/test/java/dev/hotel/web/ClientControllerTest.java
@@ -5,7 +5,7 @@ import dev.hotel.entite.Client;
 import dev.hotel.service.ClientService;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,7 +13,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import org.springframework.http.MediaType;
 
-import org.springframework.test.context.junit4.SpringRunner;
+
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@RunWith(SpringRunner.class)
+
 @WebMvcTest(ClientController.class) // contrôleur à tester
 public class ClientControllerTest {
 
